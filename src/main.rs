9c89987f23c5d1b97c88c2528eac7f86ba7cbafd
saurabh kaul <saurabh.rockstar90@gmail.com>
2021-04-07 21:58:49 +0530

    reverse integer

---
+++ src/main.rs
@@ -0,0 +1,6 @@
+mod problems;
+use problems::reverse_integer;
+
+fn main() {
+    println!("{}",reverse_integer::run(1534236469))
+}
---
+++ src/problems/reverse_integer.rs
@@ -0,0 +1,35 @@
+
+pub fn run(x: i32) -> i32 {
+    use std::fmt::Error;
+
+    let mut num = x;
+    let mut should_flip = false;
+    if num < 0{
+        num = num.abs();
+        should_flip = true;
+    }
+    let mut num_length: i32 = (num as u32).to_string().len() as i32;
+    // println!("num len is {}", num_length);
+    let mut reverse: i32 = 0;
+    let mut attempt_reverse = ||->Result<i32,Error>{
+
+        while num_length > 0 {
+            let last_digit = num % 10;
+            // println!("last digit is {}", last_digit);
+            num = num / 10;
+            num_length -= 1;
+            let subtract = (num_length as u32).checked_div(1_u32).ok_or_else(||Error)?;
+            let power = 10_i32.checked_pow(subtract).ok_or_else(||Error)?;
+            let multiply = last_digit.checked_mul(power).ok_or_else(||Error)?;
+            reverse = reverse.checked_add(multiply).ok_or_else(||Error)?;
+
+            // println!("reverse is {}", reverse);
+        }
+        if should_flip { reverse = -reverse }
+        Ok(reverse)
+    };
+    match attempt_reverse() {
+        Ok(reverse)=>reverse,
+        Err(_e)=>0
+    }
+}
