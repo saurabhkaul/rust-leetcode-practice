3b1c210b05019e818fd833613307e7bcd1059ee1
saurabh kaul <saurabh.rockstar90@gmail.com>
2024-04-19 16:11:51 +0530

    solved add_two_numbers

--- src/problems/add_two_numbers.rs
+++ src/problems/add_two_numbers.rs
@@ -1,108 +1,96 @@
 // https://leetcode.com/problems/add-two-numbers/
 
-
 // Definition for singly-linked list.
 // Definition for singly-linked list.
 #[derive(PartialEq, Eq, Clone, Debug)]
 pub struct ListNode {
-  pub val: i32,
-  pub next: Option<Box<ListNode>>
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
 }
 
 impl ListNode {
-  #[inline]
-  fn new(val: i32) -> Self {
-    ListNode {
-      next: None,
-      val
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode { next: None, val }
     }
-  }
 }
 
-pub fn iterate_over_the_lists_and_build_new(list_1: Option<Box<ListNode>>, list_2: Option<Box<ListNode>>, final_list: Box<ListNode>, carry_over:i32) ->Option<Box<ListNode>>{
-    match (list_1,list_2) {
-        (Some(node_1),Some(node_2))=>{
-            let sum = node_1.val+node_2.val + carry_over; //1..9 + 1..9
-            if sum > 9{
-                let last_digit = sum & 0xF;
-                *final_list.next = Some(
-                    Box::new(ListNode{
-                        val: last_digit,
-                        next: None,
-                    })
-
-                );
-                iterate_over_the_lists_and_build_new(node_1.next,node_2.next,final_list.next.unwrap(),1)
-            }else {
-                *final_list.unwrap().next = Some(
-                    Box::new(ListNode{
-                        val: sum,
-                        next: None,
-                    })
-
-                );
-                iterate_over_the_lists_and_build_new(node_1.next,node_2.next,final_list.next.unwrap(),0)
+fn iterate_over_the_lists_and_build_new(
+    l1: Option<Box<ListNode>>,
+    l2: Option<Box<ListNode>>,
+    carry: i32,
+) -> Option<Box<ListNode>> {
+    match (l1, l2) {
+        (Some(l1), Some(l2)) => {
+            let sum = l1.val + l2.val + carry;
+            println!(
+                "l1.val {0},l2.val {1}, carry {carry:#?}, sum {sum:#?}",
+                l1.val, l2.val
+            );
+            if sum > 9 {
+                let last_digit = sum % 10;
+                Some(Box::new(ListNode {
+                    val: last_digit,
+                    next: iterate_over_the_lists_and_build_new(l1.next, l2.next, 1),
+                }))
+            } else {
+                Some(Box::new(ListNode {
+                    val: sum,
+                    next: iterate_over_the_lists_and_build_new(l1.next, l2.next, 0),
+                }))
             }
-        },
-        (None,Some(node_2))=>{
-            let sum = node_2.val + carry_over; //0..9 + 0..9
-            if sum > 9{
-                let last_digit = sum & 0xF;
-                *final_list.unwrap().next = Some(
-                    Box::new(ListNode{
-                        val: last_digit,
-                        next: None,
-                    })
-
-                );
-                iterate_over_the_lists_and_build_new(None,node_2.next,final_list.next.unwrap(),1)
-            }else {
-                *final_list.unwrap().next = Some(
-                    Box::new(ListNode{
-                        val: sum,
-                        next: None,
-                    })
-
-                );
-                iterate_over_the_lists_and_build_new(None,node_2.next,final_list.next.unwrap(),0)
+        }
+        (Some(l1), None) => {
+            let sum = l1.val + carry;
+            println!("l1.val {0},None, carry {carry:#?}, sum {sum:#?}", l1.val);
+            if sum > 9 {
+                let last_digit = sum % 10;
+                Some(Box::new(ListNode {
+                    val: last_digit,
+                    next: iterate_over_the_lists_and_build_new(l1.next, None, 1),
+                }))
+            } else {
+                Some(Box::new(ListNode {
+                    val: sum,
+                    next: iterate_over_the_lists_and_build_new(l1.next, None, 0),
+                }))
             }
-
-        },
-        (Some(node_1),None)=>{
-            let sum = node_1.val+ carry_over; //1..9 + 1..9
-            if sum > 9{
-                let last_digit = sum & 0xF;
-                *final_list.unwrap().next = Some(
-                    Box::new(ListNode{
-                        val: last_digit,
-                        next: None,
-                    })
-
-                );
-                iterate_over_the_lists_and_build_new(node_1.next,None,final_list.next.unwrap(),1)
-            }else {
-                *final_list.unwrap().next = Some(
-                    Box::new(ListNode{
-                        val: sum,
-                        next: None,
-                    })
-
-                );
-                iterate_over_the_lists_and_build_new(node_1.next,None,final_list.next.unwrap(),0)
+        }
+        (None, Some(l2)) => {
+            let sum = l2.val + carry;
+            println!("None, l2.val {0}, carry {carry:#?}, sum {sum:#?}", l2.val);
+            if sum > 9 {
+                let last_digit = sum % 10;
+                Some(Box::new(ListNode {
+                    val: last_digit,
+                    next: iterate_over_the_lists_and_build_new(None, l2.next, 1),
+                }))
+            } else {
+                Some(Box::new(ListNode {
+                    val: sum,
+                    next: iterate_over_the_lists_and_build_new(None, l2.next, 0),
+                }))
             }
-
-        },
-        (None,None)=>{
-            Some(final_list)
-
+        }
+        //both lists have finished, but we could have a carry
+        (None, None) => {
+            if carry > 0 {
+                Some(Box::new(ListNode {
+                    val: carry,
+                    next: None,
+                }))
+            } else {
+                None
+            }
         }
     }
 }
 
-
 impl Solution {
-    pub fn add_two_numbers(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-        let final_list_init = Box::new(ListNode{ val: 0, next: None });
-        iterate_over_the_lists_and_build_new(l1,l2,final_list_init,0)
+    pub fn add_two_numbers(
+        l1: Option<Box<ListNode>>,
+        l2: Option<Box<ListNode>>,
+    ) -> Option<Box<ListNode>> {
+        iterate_over_the_lists_and_build_new(l1, l2, 0)
     }
 }
--- src/problems/mod.rs
+++ src/problems/mod.rs
@@ -1,3 +1,3 @@
+mod add_two_numbers;
 pub mod remove_n_from_end_link_list;
 pub mod reverse_integer;
-mod add_two_numbers;
