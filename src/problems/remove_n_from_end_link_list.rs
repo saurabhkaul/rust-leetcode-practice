17997149d3ccf5ff69d6ec0006ec263714444394
saurabh kaul <saurabh.rockstar90@gmail.com>
2024-04-23 21:32:03 +0530

    adding solution

--- src/problems/remove_n_from_end_link_list.rs
+++ src/problems/remove_n_from_end_link_list.rs
@@ -1,20 +1,53 @@
-// // Definition for singly-linked list.
-// #[derive(PartialEq, Eq, Clone, Debug)]
-// struct ListNode<K> {
-//     pub val: K,
-//     pub next: Option<ListNode<K>>,
-// }
-//
-// pub enum LinkList<K> {
-//     None,
-//     LinkList { root_node: ListNode<K> },
-// }
-// impl<K> LinkList<K> {
-//     fn new() -> Self<K> {
-//         Self
-//     }
-//     fn add_node(&self, value: K) {}
-//     fn from_arr(arr: &[K]) -> LinkList<K> {
-//         LinkList
-//     }
-// }
+//https://leetcode.com/problems/remove-nth-node-from-end-of-list/description/
+
+
+// Definition for singly-linked list.
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+  pub val: i32,
+  pub next: Option<Box<ListNode>>
+}
+
+impl ListNode {
+  #[inline]
+  fn new(val: i32) -> Self {
+    ListNode {
+      next: None,
+      val
+    }
+  }
+}
+
+
+
+
+fn remove_nth_from_end_recur(left: &Option<Box<ListNode>>, right: &Option<Box<ListNode>>) ->Option<Box<ListNode>>{
+    match (left,right) {
+        (Some(left), Some(right)) => {
+            Some(Box::new(ListNode{
+                val: left.val,
+                next: remove_nth_from_end_recur(&left.next,&right.next),
+            }))
+        },
+        (Some(l),None)=>{
+            l.next.clone()
+        }
+        _ => unreachable!()
+    }
+}
+
+
+
+impl Solution {
+    pub fn remove_nth_from_end(head: Option<Box<ListNode>>, n: i32) -> Option<Box<ListNode>> {
+        //create our right pointer
+        let left = &head;
+        let mut right = &head;
+        for _ in 0..n{
+            if let Some(node) = right{
+                right = &node.next;
+            }
+        }
+        remove_nth_from_end_recur(left,right)
+    }
+}
